83a9dcfa9388562ba6dd537c24e4bbfe39a17adb
Wesley Raphael Jr <wesleyraphaeljr@gmail.com>
2015-05-24 14:02:51 -0700

    r file completed according to instructions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below serve to "process" a matrix so that 
+## an inverse is outputted and stored for future use
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function is essentially a holding pen;
+## It's contents are later referenced by the cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) inv <<- inverse
+	getinverse <- function() inv
+	list(set = set, get = get, 
+	    setinverse = setinverse, 
+	    getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function searches the makeCacheMatrix function
+## to see if the desired inverse is stored. If not, it computes it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+	if (!is.null(inv)) {
+		message("retrieving cached inverse")
+		return(inv)
+	}
+	matrix <- x$get()
+	inv <- solve(matrix)
+	x$setinverse(inv)
+	inv
 }
+
+## In practice, it would probably be best to stitch the two functions
+## together so that all a person had to do was input a matrix and the
+## program would figure out whether the corresponding inverse had been
+## stored or if it needed to be computed. Since we were given a stub
+## to start out with, I tried to adhere to the format the instructions
+## seemed to put forth.
